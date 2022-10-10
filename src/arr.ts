bce2e82a9447dc4d0b8c87866b3cd4690ecbaf10
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-10-10 22:52:41 +0300

    WIP

--- src/arr.ts
+++ src/arr.ts
@@ -1,37 +1,33 @@
-
-let number = [10,45,25,36,99,9,45,1,2,4,5,6,]
+let number = [10, 45, 25, 36, 99, 9, 45, 1, 2, 4, 5, 6];
 
 number.push(10000);
 
 console.log(number);
 
-
 // to show mix array
 
-const mix = ["name",10,25,65,98,7,8,"YAZ",false]
+const mix = ["name", 10, 25, 65, 98, 7, 8, "YAZ", false];
 
 mix.push(45);
 
-
 // mixed type
 
-const mixed: any[] = ["Jonson",10,45,8,9,6,false];
+const mixed: any[] = ["Jonson", 10, 45, 8, 9, 6, false];
 
 console.log(mixed);
 
-for (let result of mixed){
-   console.log(result);
+for (let result of mixed) {
+  console.log(result);
 }
 
+const listItem: number[] = [45, 10, 45, 8, 9, 6, 89, 78, 9, 6, 5, 6, 5, 5];
 
-const listItem: number[] = [45,10,45,8,9,6,89,78,9,6,5,6,5,5];
-
-
-for (let i of listItem){
-   console.log("List resutl is here:"+i);
+for (let i of listItem) {
+  console.log("List resutl is here:" + i);
 }
 
+const namelIST: string[] = ["Jon", "Doe", "Mikle"];
 
-
-const namelIST: string[] = ["Jon",'Doe',"Mikle"];
-
+for (let n of namelIST) {
+  console.log(n);
+}
